efde7a9d8719aca435d54300d1511fa8ac994369
Daniel Danielski <105228278+ddanielski@users.noreply.github.com>
2022-12-07 22:46:01 -0330

    day7...

--- day7/day7.py
+++ day7/day7.py
@@ -1,53 +1,54 @@
+from collections import defaultdict
 import os
-import re
 
 
-def get_dir_info(lines):
-    dir_tree = {}
-    file = {}
-    tmp_dir = []
-    for line in lines:
-        if "cd" in line:
-            dir_name = line.split("cd ", 1)[1]
-            if ".." in dir_name:
-                tmp_dir.pop()
-            else:
-                dir_name = dir_name.rstrip()
-                if tmp_dir:
-                    dir_tree[tmp_dir[-1]].append(dir_name)
-                if dir_name is not dir_tree:
-                    dir_tree[dir_name] = []
-                    file[dir_name] = []
-                tmp_dir.append(dir_name)
-        elif "dir" or "$" not in line:
-            file_size = line.split(" ", 1)[0]
-            if file_size.isdigit():
-                file[tmp_dir[-1]].append(int(file_size))
+def part1(file_sz, const):
+    sum_files = 0
+    for sz in file_sz.values():
+        if sz <= const:
+            sum_files += sz
+    return sum_files
 
-    print(file)
-    print(dir_tree)
-    return dir_tree, file
+
+def part2(file_sz, const):
+    files_sz = []
+    for sz in file_sz.values():
+        if sz >= const:
+            files_sz.append(sz)
+    return min(files_sz)
 
 
 def main():
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
 
-    with open(os.path.join(__location__, 'day7_sample.txt')) as f:
+    with open(os.path.join(__location__, 'day7.txt')) as f:
         lines = f.readlines()
-        dir_tree, file_sz = get_dir_info(lines)
-        dir_sz = {}
-        for dir in dir_tree:
-            dir_sz[dir] = sum(file_sz[dir])
+        file_sz = defaultdict(int)
+        dir = []
 
-        delta = 1
-        while(delta > 0):
-            for dir, sub_dir in dir_tree.items():
-                if sub_dir:
-                    dir_sz[dir] += sum(dir_sz[sub_dir])
-            delta = 0
+        for line in lines:
+            line = line.strip()
+            if "$ cd" in line:
+                # We want the third field.  We can't separate with "cd ", as can be a file name
+                dir_name = line.split()[2]
+                if ".." in line:
+                    dir.pop()
+                elif "/" in dir_name:
+                    dir.append("/")
+                else:
+                    dir.append(
+                        f"{dir[-1]}{'/' if dir[-1] != '/' else ''}{dir_name}")
+            elif "dir" or "$" not in line:
+                file_size = line.split(" ", 1)[0]
+                if file_size.isdigit():
+                    for p in dir:
+                        file_sz[p] += int(file_size)
 
-        print(dir_sz)
+    print("Part I: The sum of all the dir with at most 100k is " +
+          str(part1(file_sz, 100000)))
+    print("Part II: You need to remove the directory of size " +
+          str(part2(file_sz, 30000000 - (70000000 - file_sz['/']))))
 
 
 if __name__ == "__main__":
